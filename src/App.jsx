c2418824de6dd7ef211c9b0397561b053a5af0e9
js976999 <js976999@gmail.com>
2025-08-05 07:50:53 +1000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import RandomNameDrawApp from "./RandomNameDrawApp";
+import "./RandomNameDrawApp.css";
+
+export default function App() {
+  return <RandomNameDrawApp />;
+}
