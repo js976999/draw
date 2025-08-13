a106718ff2037b92e7d69eb3f79bf949331e5f72
js976999 <js976999@gmail.com>
2025-08-13 16:44:13 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ export default function App() {
     <div>
       <h1>Deployment Test: {Date.now()}</h1>
       <RandomNameDrawApp />
+      <div>TESTING - 2025-08-13 - HELLO WORLD</div>
     </div>
   );
 }
